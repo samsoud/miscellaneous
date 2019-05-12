c3689e2844009a6e539e01537311d5526e444437
unknown <sam.soud@gmail.com>
2019-05-11 22:32:01 -0400

    initial commit

---
+++ find-replace.ps1
@@ -0,0 +1,14 @@
+﻿$find = 'https://cloudengartifacts.blob.core.windows.net/templates'
+$replace = 'https://cloudengartifacts.blob.core.windows.net/templates'
+$match =  '*.json'
+$preview = $true
+
+foreach ($sc in dir -recurse -include $match | where { test-path $_.fullname -pathtype leaf} ) {
+    select-string -path $sc -pattern $find
+    if (!$preview) {
+       (get-content $sc) | foreach-object { $_ -replace $find, $replace } | set-content $sc
+    }
+}
+
+
+https://samsoudstorage123.blob.core.windows.net/container01
