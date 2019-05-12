cb56e9b25c0bbc37486fee739cdf4e95b6c59ea4
unknown <sam.soud@gmail.com>
2019-05-11 22:37:42 -0400

    find-replace.ps1

--- get-AzureSASTokenBlobs.ps1
+++ get-AzureSASTokenBlobs.ps1
@@ -1,16 +1,24 @@
 ﻿  # Set target variables
-    $subscriptionName = 'GUIDES-F-DEV'
-    $resourceGroupName = 'ggcrmbincne'
-    $storAcctName = 'teststor43899009'
-    $blobContainerName = 'blobContainerName234'
-	SubscriptionName='Pay-as-you-go'
+    $subscriptionName = 'Pay-As-You-Go Dev/Test'
+    $username="sam.soud@gmail.com"
+    $SubscriptionID="81a9ea66-6119-4aa3-8322-fd4abfd5faae"
+    Connect-AzureRmAccount -SubscriptionID $SubscriptionID
+    
+    $Subscription = Get-AzureRmSubscription -SubscriptionID $SubscriptionID | Select-AzureRmSubscription
+    Select-AzureRmSubscription -SubscriptionID $SubscriptionID | Set-AzureRmContext
+    $resourceGroupName = 'test1'
+    $storAcctName = 'storageaccount1235'
+    $blobContainerName = 'container01'
 	$username="sam.soud@gmail.com"
-	Login-AzureRmAccount -SubscriptionName $SubscriptionName
-	Get-AzureRmSubscription -SubscriptionName $SubscriptionName | Select-AzureRmSubscription
-	Select-AzureRmSubscription -SubscriptionName $SubscriptionName | Set-AzureRmContext
+    
+    
+    #Login-AzureRmAccount -SubscriptionName $SubscriptionName
+	#Login-AzureRmAccount -SubscriptionName $SubscriptionName
+	#Get-AzureRmSubscription -SubscriptionName $SubscriptionName | Select-AzureRmSubscription
+	#Select-AzureRmSubscription -SubscriptionName $SubscriptionName | Set-AzureRmContext
 
 
-function Get-AzureSASTokenDynamicsChinaBlobs {
+function Get-AzureSASTokenDynamicsBlobs {
 
     [CmdletBinding()]
     param()
@@ -20,43 +28,48 @@ function Get-AzureSASTokenDynamicsChinaBlobs {
     Write-Verbose "ASM - Checking credentials and setting subscription to: $subscriptionName"
     try {
 
-        Select-AzureSubscription -SubscriptionName $subscriptionName -Current -ErrorAction Stop > $null
+        Select-AzureRmSubscription -SubscriptionID $SubscriptionID | Set-AzureRmContext
     
     } catch {
 
-        Add-AzureAccount -Environment azurechinacloud > $null
-        Select-AzureSubscription -SubscriptionName $subscriptionName -Current > $null
+        #Add-AzureAccount -Environment azurechinacloud > $null
+          Select-AzureRmSubscription -SubscriptionID $SubscriptionID | Set-AzureRmContext
     }
     
     Write-Verbose "ARM - Checking credentials and setting subscription to: $subscriptionName"
     try {
 
-        Set-AzureRmContext -SubscriptionName $subscriptionName -ErrorAction Stop | Out-Null
+        Set-AzureRmContext -SubscriptionID $SubscriptionID -ErrorAction Stop | Out-Null
     
     } catch {
         
        Login-AzureRmAccount -EnvironmentName azurechinacloud | Out-Null
-       Set-AzureRmContext -SubscriptionName $subscriptionName | Out-Null        
+       Set-AzureRmContext -SubscriptionID $SubscriptionID | Out-Null        
         
     }
 
     Write-Verbose "Getting ARM Storage Account Key for $storAcctName"
-    $storAcct = (Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName).where({$PSITEM.StorageAccountName -eq $storAcctName})
-    $storKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $storAcct.ResourceGroupName -Name $storAcct.StorageAccountName).where({$PSItem.KeyName -eq 'key1'})
-
+    $storAcct = (Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -AccountName $storAcctName)
+    $storKey1 = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storAcctName).Value[0]
+    $storKey2 = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storAcctName).Value[1]
     Write-Verbose "Creating new ASM storage context"
-    $storContext = New-AzureStorageContext -StorageAccountName $storAcct.StorageAccountName -StorageAccountKey $storKey.Value
-
+    #$storContext = New-AzureStorageContext -StorageAccountName  $storAcctName -StorageAccountKey $storKey1
+    #$storContext = New-AzureStorageContext -StorageAccountName  $storAcctName -StorageAccountKey $storKey2
     Write-Verbose "Getting list of blobs in $blobContainerName"
-    $blobContainer = Get-AzureStorageContainer -Name $blobContainerName -Context $storContext
-    $blobNames = $blobContainer.CloudBlobContainer.ListBlobs().Name 
+    #$blobContainer = Get-AzureStorageContainer -Name $blobContainerName -Context $storContext
+    $blobContainers = Get-AzureStorageContainer -Context $storContext
+    $Containers=$blobContainer.CloudBlobContainer.Name
+
+    ###
 
     Write-Verbose "Blobs are: $blobNames"
 
-    foreach ($blobName in $blobNames) {
+    foreach ($Container in $Containers) {
 
-        Write-Verbose "Generating new SAS token for blob: $blobName"
-        $SASTokenBlob = New-AzureStorageBlobSASToken -Context $storContext -Container $blobContainer.Name -Blob $blobName -StartTime (Get-Date).AddMinutes(-15) -ExpiryTime (Get-Date).AddDays(1) -Permission rwd
+        Write-Verbose "Generating new SAS token for blob: $Container"
+        $SASTokenBlob =New-AzureStorageBlobSASToken -Container $Container -Blob $blob -Permission rwd
+
+        New-AzureStorageBlobSASToken -Container "ContainerName" -Blob "BlobName" -Permission rwd
     
         # create URIs for the blob
         $baseURI = "https://$storAcctName.blob.core.chinacloudapi.cn/$blobContainerName"
